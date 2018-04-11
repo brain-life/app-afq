30dc91bc8cb2c83fcab860eb4692c442a81916b7
Soichi Hayashi <hayashis@iu.edu>
2018-04-11 12:32:08 -0400

    added mrDiffusion/templates to compiled binary

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ addpath(genpath('/N/soft/rhel7/spm/8'))
 addpath(genpath('/N/u/brlife/git/vistasoft'))
 addpath(genpath('/N/u/brlife/git/afq'))
 
-mcc -m -R -nodisplay -d compiled main
+mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
 exit
 END
 matlab -nodisplay -nosplash -r build && rm build.m
