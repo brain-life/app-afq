8402b21c783550ef646265a29150b298f892f3c6
Soichi Hayashi <hayashis@iu.edu>
2018-04-10 22:29:27 -0400

    recompiled main

--- build.m
+++
@@ -1,7 +0,0 @@
-addpath(genpath('/N/u/brlife/git/jsonlab'))
-addpath(genpath('/N/soft/rhel7/spm/8'))
-addpath(genpath('/N/u/brlife/git/vistasoft'))
-addpath(genpath('/N/u/brlife/git/afq'))
-
-mcc -m -R -nodisplay -d compiled main
-exit
