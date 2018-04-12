a669ff9c25d7c215fc1f5f4108fc016046a335ef
Soichi Hayashi <hayashis@iu.edu>
2018-04-11 22:28:36 -0400

    switched back to use lindsey's patched vistasoft

--- compile.sh
+++ compile.sh
@@ -4,7 +4,10 @@ module load matlab/2017a
 cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/rhel7/spm/8'))
-addpath(genpath('/N/u/brlife/git/vistasoft'))
+
+%addpath(genpath('/N/u/brlife/git/vistasoft'))
+addpath(genpath('/N/u/kitchell/Karst/Applications/vistasoft'))
+
 addpath(genpath('/N/u/brlife/git/afq'))
 
 mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
