39ee99841648e8f2f26368a7bf303f69d343115c
Soichi Hayashi <hayashis@iu.edu>
2018-04-12 15:46:20 -0400

    recompiled main

--- compile.sh
+++ compile.sh
@@ -5,12 +5,13 @@ cat > build.m <<END
 addpath(genpath('/N/u/brlife/git/jsonlab'))
 addpath(genpath('/N/soft/rhel7/spm/8'))
 
-%addpath(genpath('/N/u/brlife/git/vistasoft'))
-addpath(genpath('/N/u/kitchell/Karst/Applications/vistasoft'))
+%my local branch has https://github.com/vistalab/vistasoft/pull/287
+addpath(genpath('/N/u/hayashis/git/vistasoft'))
 
 addpath(genpath('/N/u/brlife/git/afq'))
 
-mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
+%mcc -m -R -nodisplay -a /N/u/brlife/git/vistasoft/mrDiffusion/templates -d compiled main
+mcc -m -R -nodisplay -d compiled main
 exit
 END
 matlab -nodisplay -nosplash -r build && rm build.m
