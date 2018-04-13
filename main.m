3170f322a32a657e4bd109f67c2a4ac2d1b7faef
Soichi Hayashi <hayashis@iu.edu>
2018-04-13 15:57:04 +0000

    updated to allow both control and patient

--- main.m
+++ main.m
@@ -6,26 +6,32 @@ if ~isdeployed()
             disp('loading paths for IUHPC')
             addpath(genpath('/N/u/brlife/git/jsonlab'))
             addpath(genpath('/N/soft/rhel7/spm/8'))
-            %addpath(genpath('/N/u/kitchell/Karst/Applications/vistasoft'))
-            %addpath(genpath('/N/u/kitchell/Karst/Applications/AFQ'))
-            addpath(genpath('/N/u/brlife/git/vistasoft'))
-            addpath(genpath('/N/u/brlife/git/afq'))
+            addpath(genpath('/N/u/kitchell/Karst/Applications/vistasoft'))
+            addpath(genpath('/N/u/kitchell/Karst/Applications/AFQ'))
+            %addpath(genpath('/N/u/brlife/git/vistasoft'))
+            %addpath(genpath('/N/u/brlife/git/afq'))
         case 'VM'
             disp('loading paths for Jetstream VM')
-            %addpath(genpath('/usr/local/jsonlab'))
-            %addpath(genpath('/usr/local/spm8'))
-            %addpath(genpath('/usr/local/vistasoft'))
-            %addpath(genpath('/usr/local/afq-master'))
+            addpath(genpath('/usr/local/jsonlab'))
+            addpath(genpath('/usr/local/spm8'))
+            addpath(genpath('/usr/local/vistasoft'))
+            addpath(genpath('/usr/local/afq'))
     end
 end
 
 % load my own config.json
 config = loadjson('config.json');
+disp(config)
 
-% run AFQ
-sub_dirs = { [config.dtiinit '/dti'] };
-sub_group = [0]; %0 = control 1 = patient
+sub_dirs = { [config.patient '/dti'], [config.control '/dti'] };
+%0 = control 1 = patient
+sub_group = [1, 0]; 
+disp(sub_dirs);
+disp(sub_group);
+
+disp('running afq_Create');
 afq = AFQ_Create('sub_dirs', sub_dirs, 'sub_group', sub_group);
+disp('running afq_run');
 [afq patient_data control_data norms abn abnTracts] = AFQ_run(sub_dirs, sub_group, afq);
 save('afq.mat', 'afq');
 
